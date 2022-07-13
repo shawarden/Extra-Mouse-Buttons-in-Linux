458316400b68fb6a0cfec8af7ed5509a6ad2dd84
Samuel Hawarden <shawarden@users.noreply.github.com>
2022-07-14 09:13:57 +1200

    script folder

--- data/Games/Fallen Order/Force Pull.py
+++ data/Games/Fallen Order/Force Pull.py
@@ -9,7 +9,7 @@ if not store.has_key("pulling"):
 if store.get_value("pulling") == 0:
     store.set_value("pulling",1)
     keyboard_actual.press('3')
-    os.system('~/.config/autokey/keyrelease.sh 88')
+    os.system('~/.config/autokey/scripts/keyrelease.sh 88')
     store.set_value("pulling",0)
     keyboard_actual.release('3')
 
