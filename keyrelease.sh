b4d85a04d467fe10cc4cf55814f326b26e47cf4e
Samuel Hawarden <hawarden+github@gmail.com>
2022-07-13 15:20:12 +1200

    comments

--- keyrelease.sh
+++ keyrelease.sh
@@ -1,17 +1,30 @@
 #!/bin/bash
 
+# What key to watch for
 keyNum=$1
 
+# Loop until there are no $keyNum in the down state.
+#	Get list of all xinput devices.
+#		Skip pointers and master.
+#		Strip everything before the id=
+#		Strip everything after the id number.
+#	Why oh why can't we just poll the root/master keyboard?
+#	This wont work on wayland cuz no xinput
 while for id in $(
 		xinput list | \
 		grep -v "pointer\|master" | \
-		grep keyboard | \
 		awk -F'id=' '{print $2}' | \
 		awk '{print $1}'
 		)
 	do
-		xinput query-state $id
-	done | grep down | grep ${keyNum} &>/dev/null
+		# Dump device state
+		#	Pull out first instance of target key down
+		xinput query-state $id | grep -P "key\[${keyNum}?\]=down" && break
+	done | grep "down" &>/dev/null
 do
+		# As long as there's an active 'down' state, twiddle your thumbs.
 		continue
 done
+
+# No down states found.
+# Target key must be up.
