c3a890c3088fb1ea5b0dc5a676f45be8a34a921b
Samuel Hawarden <hawarden+github@gmail.com>
2022-07-14 15:14:49 +1200

    autokey kinda sucks

--- data/Apps/Terminals/Force Push.py
+++ data/Apps/Terminals/Force Push.py
@@ -3,47 +3,25 @@
 from os import system
 from pynput.keyboard import Controller
 from keystates import waitForKeyRelease
-import logging, sys, subprocess
 
-bindKey='m'
+bindKey='1'
 outputKey='2'
 isNumpad=False
 storeVal='isPressed'
 
-logging.basicConfig(
-    format='%(asctime)s %(levelname)s %(message)s',
-    level=logging.DEBUG,
-    stream=sys.stdout
-)
-
 keyboard_actual = Controller()
 
 if not store.has_key(storeVal):
-    logging.debug("AK - Store {0} doesn't exist.".format(storeVal))
     store.set_value(storeVal,False)
-else:
-    logging.debug("AK - Store {0} already exists.".format(storeVal))
 
 if not store.get_value(storeVal):
-    logging.debug("AK - Store {0} says we're not pressing!".format(storeVal))
-
-    logging.debug("AK - Store pressed to {0}.".format(storeVal))
+    print('---- PRESS ----')
     store.set_value(storeVal,True)
-    logging.debug("AK - Press Key {0}".format(outputKey))
     keyboard_actual.press(outputKey)
-
-    logging.debug("AK - Wait for Key {0} release.".format(bindKey))
-    #system('~/.config/autokey/scripts/keyrelease.sh 87')
-    waitForKeyRelease(bindKey,isNumpad)
-
-    logging.debug("AK - Store release to {0}".format(storeVal))
+    system('~/.config/autokey/scripts/keyrelease.sh 87')
+    #waitForKeyRelease(bindKey,isNumpad)
     store.set_value(storeVal,False)
-    logging.debug("AK - Release key {0}".format(outputKey))
     keyboard_actual.release(outputKey)
+    print('---- RELEASE ----')
 else:
-    logging.debug("AK - Key {0} is already down. STOP SPAMMING!".format(bindKey))
-
-
-
-
-
+    print('---- SPAM ----')
--- scripts/keystates.py
+++ scripts/keystates.py
@@ -1,61 +1,30 @@
 #!/usr/bin/env python3
 
 from pynput import keyboard
-import logging, sys, subprocess
-
-logging.basicConfig(
-    format='%(asctime)s %(levelname)s %(message)s',
-	level=logging.DEBUG,
-	stream=sys.stdout
-)
 
 def waitForKeyRelease(realKey,bNumpadOnly=False):
-    """Wait for the desired key to be released"""
-
     def isNumberPad(key):
-        """Check if key is numpad/mapped"""
-
         if hasattr(key, 'vk'):
-            logging.info("KS - Key {0} has vk!".format(key))
             if key.vk is None:
-                logging.info("KS - KEy {0}s vk is {1}".format(key.vk))
                 return True
             else:
-                logging.info("KS - Key {0} is not a numpad/mapped".format(key))
                 return False
         else:
-            logging.info("KS - Key {0} does not have vk".format(key))
             return False
 
     def on_release(key):
-        """Exits listener on key release"""
-
         nonlocal realKey
         nonlocal bNumpadOnly
-        logging.info("KS - Targets: Key {0}, Numpad/mapped: {1}".format(realKey, bNumpadOnly))
 
         if hasattr(key, 'char'):
-            logging.info("KS - Key {0} is char key {1}.".format(key, key.char))
             if key.char == realKey:
-                logging.info("KS - Key {0} is target Key {1}".format(key.char,realKey))
                 if bNumpadOnly:
-                    logging.info("KS - We want numpad/mapped key!")
                     if isNumberPad(key):
-                        logging.info("KS - Key {0} is numpad/mapped!".format(key))
                         return False
-                    else:
-                        logging.info("KS - Key {0} is NOT numpad/mapped!".format(key))
                 else:
-                    logging.info("KS - Don't care if {0} is numpad/mapped!".format(key))
                     return False
-            else:
-                logging.info("KS - Key {0} is not target key {1}!".format(key,realKey))
-        else:
-            logging.info("KS - Key {0} does not have vk attribute!".format(key))
 
-    logging.info("KS - Watch for key {0}:{1} release trigger!".format(realKey,bNumpadOnly))
     with keyboard.Listener(
         on_release=on_release
     ) as listener:
-        listener.join(2)
-    logging.info("KS - Key {0}:{1} released!".format(realKey,bNumpadOnly))
+        listener.join()
