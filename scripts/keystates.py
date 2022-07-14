222c0365f167abbdd9ccfdc5e8829d8df520ec45
Samuel Hawarden <hawarden+github@gmail.com>
2022-07-14 14:31:03 +1200

    release trigger

--- scripts/keystates.py
+++ scripts/keystates.py
@@ -6,57 +6,56 @@ import logging, sys, subprocess
 logging.basicConfig(
     format='%(asctime)s %(levelname)s %(message)s',
 	level=logging.DEBUG,
-	stream=sys.stderr
+	stream=sys.stdout
 )
 
 def waitForKeyRelease(realKey,bNumpadOnly=False):
-    logging.info("Wait for the desired key to be released")
+    """Wait for the desired key to be released"""
 
     def isNumberPad(key):
-        logging.info("Check if key is numpad/mapped")
+        """Check if key is numpad/mapped"""
 
-        logging.info("Does key have virtual key attribute?")
         if hasattr(key, 'vk'):
-            logging.info("YES! Is that virtual key empty/None?")
+            logging.info("KS - Key {0} has vk!".format(key))
             if key.vk is None:
-                logging.info("YES! It's numpad/mapped")
+                logging.info("KS - KEy {0}s vk is {1}".format(key.vk))
                 return True
-            logging.info("NO! It's not a numpad/mapped")
+            else:
+                logging.info("KS - Key {0} is not a numpad/mapped".format(key))
+                return False
+        else:
+            logging.info("KS - Key {0} does not have vk".format(key))
             return False
-        logging.info("NO!")
-        return False
 
     def on_release(key):
-        logging.info("Exits listener on key release")
+        """Exits listener on key release"""
 
         nonlocal realKey
-        logging.info("Target key: "+realKey)
         nonlocal bNumpadOnly
-        logging.info("Numpad/mapped key? "+bNumpadOnly)
+        logging.info("KS - Targets: Key {0}, Numpad/mapped: {1}".format(realKey, bNumpadOnly))
 
-        logging.info("Is this a char key?")
         if hasattr(key, 'char'):
-            logging.info("YES! Does our key match?")
+            logging.info("KS - Key {0} is char key {1}.".format(key, key.char))
             if key.char == realKey:
-                logging.info("YES! Are we after numpad/mapped?")
+                logging.info("KS - Key {0} is target Key {1}".format(key.char,realKey))
                 if bNumpadOnly:
-                    logging.info("YES! Is this key numpad/mapped?")
+                    logging.info("KS - We want numpad/mapped key!")
                     if isNumberPad(key):
-                        logging.info("YES! Exit listener")
+                        logging.info("KS - Key {0} is numpad/mapped!".format(key))
                         return False
                     else:
-                        logging.info("NO! Not numbpad/mapped.")
+                        logging.info("KS - Key {0} is NOT numpad/mapped!".format(key))
                 else:
-                    logging.info("NO! Exit listener")
+                    logging.info("KS - Don't care if {0} is numpad/mapped!".format(key))
                     return False
             else:
-                logging.info("NO! Keep waiting.")
+                logging.info("KS - Key {0} is not target key {1}!".format(key,realKey))
         else:
-            logging.info("NO! Need to handle other keys!")
+            logging.info("KS - Key {0} does not have vk attribute!".format(key))
 
-    logging.info("Watch for key release trigger")
+    logging.info("KS - Watch for key {0}:{1} release trigger!".format(realKey,bNumpadOnly))
     with keyboard.Listener(
         on_release=on_release
     ) as listener:
-        listener.join()
-    logging.info("Key released!"
+        listener.join(2)
+    logging.info("KS - Key {0}:{1} released!".format(realKey,bNumpadOnly))
